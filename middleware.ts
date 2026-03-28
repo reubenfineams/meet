4e25f2f25cb7eab19e199258a5ac91d475434fd5
Reuben Fine <fine@allmysons.com>
2026-03-27 20:20:48 -0500

    rooms adjust again

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,22 @@
-export { auth as middleware } from '@/auth';
+import NextAuth from 'next-auth';
+import MicrosoftEntraID from 'next-auth/providers/microsoft-entra-id';
+
+const { auth } = NextAuth({
+  providers: [
+    MicrosoftEntraID({
+      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+      issuer: `https://login.microsoftonline.com/${process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID}/v2.0`,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth }) {
+      return !!auth;
+    },
+  },
+});
+
+export default auth;
 
 export const config = {
   matcher: ['/rooms/:path*', '/api/connection-details'],
